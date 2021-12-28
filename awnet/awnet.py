91e2020650ef2a6c6ec75c1094d843b09688de46
tlskinneriv <tlskinneriv@users.noreply.github.com>
2021-12-28 15:38:32 -0600

    change to mqtt

--- awnet/awnet.py
+++ awnet/awnet.py
@@ -7,33 +7,39 @@
 # publish date: 2021-03-20
 
 
-from urllib.parse import urlparse, parse_qs
+from urllib.parse import parse_qs, quote
 import json
 import requests
-import time, os
+import os
 
 # set MQTT vars
-ENTITY_ID = os.getenv("ENTITY_ID", "station")
-# PUBLISH_ALL = bool(os.getenv('PUBLISH_ALL',"false"))
-AUTH_TOKEN = os.getenv("SUPERVISOR_TOKEN", "")
+AUTH_TOKEN = os.getenv("SUPERVISOR_TOKEN", "test")
+ID_KEY = os.getenv("ID_KEY", "PASSKEY")
 
+def publish_mqtt(subtopic, id, payload):
+    url_id = quote(id)
+    topic = f"awnet/{subtopic}/{url_id}"
+    payload_json = json.dumps(payload)
 
-def publish(entity, msg):
     head = {
         "Authorization": "Bearer " + AUTH_TOKEN,
         "content-type": "application/json",
     }
     good_responses = [200, 201]
 
-    url = "http://supervisor/core/api/states/" + "sensor." + entity
+    url = "http://supervisor/core/api/mqtt/publish"
 
-    response = requests.post(url, data=msg, headers=head)
+    print(url)
+    print(payload_json)
+    print(head)
+
+    response = requests.post(url, data=payload_json, headers=head)
 
     if response.status_code in good_responses:
-        # print(f"Sent {msg} to topic {topic}")
+        print(f"Sent {payload_json} to topic {topic}")
         pass
     else:
-        print(f"Failed to send {entity} update to topic HASS")
+        print(f"Failed to send {topic} update")
 
 
 def handle_results(result):
@@ -41,50 +47,26 @@ def handle_results(result):
     stationtype: ['AMBWeatherV4.2.9'], PASSKEY: ['<station_mac_address>'], dateutc: ['2021-03-20 17:12:27'], tempinf: ['71.1'], humidityin: ['36'], baromrelin: ['29.693'],    baromabsin: ['24.549'],    tempf: ['58.8'], battout: ['1'], humidity: ['32'], winddir: ['215'],windspeedmph: ['0.0'],    windgustmph: ['0.0'], maxdailygust: ['3.4'], hourlyrainin: ['0.000'],    eventrainin: ['0.000'],    dailyrainin: ['0.000'],
     weeklyrainin: ['0.000'], monthlyrainin: ['0.000'], totalrainin: ['0.000'],    solarradiation: ['121.36'],
     uv: ['1'],batt_co2: ['1']"""
-
     # This changes the reulting key values from lists to single values
     for key in result:
         if len(result[key]) == 1:
             result[key] = result[key][0]
-
-    output_dict = {}
-
-    state = "online"
-
-    output_dict["state"] = state
-
-    output_dict["attributes"] = result
-
-    json_result = json.dumps(output_dict)
-
-    publish(ENTITY_ID, json_result)
-
-
-#    if PUBLISH_ALL == True:
-
-#        for key in output_dict["attributes"]:
-
-#            entity = ENTITY_ID + "_" + key
-#            entity_json = json.dumps({ "state": output_dict["attributes"][key], "attributes": { "dateutc": output_dict["attributes"]["dateutc"] }})
-#            publish(entity, entity_json)
+        else:
+            print('unexpected list size for key {}'.format(key))
+    publish_mqtt('station', result.get(ID_KEY, 'invalid_id'), result)
 
 
 def application(environ, start_response):
     # the FQDN (i.e. /data?stationtype=AMBWeatherV4.2.9&&tempinf=71.1&humidityin=35)
     # adapted the code to reflect comment from original blog post.
-
     result = parse_qs(environ["QUERY_STRING"])
-
     # send to our other function to deal with the results.
     # result is a dict
     handle_results(result)
-
     # we need to return a response. HTTP code 200 means everything is OK. other HTTP codes include 404 not found and such.
     start_response("200 OK", [("Content-Type", "text/plain")])
-
     # the response doesn't actually need to contain anything
     response_body = ""
-
     # return the encoded bytes of the response_body.
     # for python 2 (don't use python 2), the results don't need to be encoded
     return [response_body.encode()]
